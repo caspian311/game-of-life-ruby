f9846a5040f9af25bb93fd7f20597bddbe76eb9e
Matt Todd <matt.c.todd@gmail.com>
2015-07-17 22:17:35 -0500

    clean up

--- lib/console.rb
+++ lib/console.rb
@@ -7,12 +7,10 @@ class Console
    def show(board)
       message = ''
       board.size.times do |x|
-         #message += '|'
          board.size.times do |y|
             message += board.is_alive?(x, y) ? '*' : ' '
          end
          message += "\n"
-         #message += "|\n"
       end
       @output.print "#{message}\n"
    end
