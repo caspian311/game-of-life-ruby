cf196fd9609cbddfdb360775d906e383e0b07829
Matt Todd <matt.c.todd@gmail.com>
2015-08-22 18:44:30 -0500

    refactored the nasty nested if

--- lib/board_transformer.rb
+++ lib/board_transformer.rb
@@ -6,19 +6,22 @@ class BoardTransformer
 
       previous_board.size.times do |x|
          previous_board.size.times do |y|
-            number_of_neighbors = NeighborCounter.new.count_neighbors previous_board, x, y
-            if previous_board.is_alive? x, y
-               if number_of_neighbors == 2 || number_of_neighbors == 3
-                  new_board.revive x, y
-               end
-            else
-               if number_of_neighbors == 3
-                  new_board.revive x, y
-               end
+            if should_revive? previous_board, x, y
+               new_board.revive x, y
             end
          end
       end
 
       new_board
+   end
+
+   private
+   def should_revive?(previous_board, x, y)
+      number_of_neighbors = NeighborCounter.new.count_neighbors previous_board, x, y
+      if number_of_neighbors == 3
+         return true
+      end
+
+      previous_board.is_alive? x, y and number_of_neighbors == 2
    end
 end
